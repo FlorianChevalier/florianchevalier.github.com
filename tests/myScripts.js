4a104a0422b83a122d4b678d5fcd9bdbdcaedcbf
FlorianChevalier <55292112+FlorianChevalier@users.noreply.github.com>
2020-01-15 13:26:52 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -6,8 +6,8 @@ for (n= 0; n<9; n++){
   document.write("random:" + ans + "<br/>");
 }
 document.write("Square loop <br/>");
-var l;
-for(l= 2; 1<10; l++){
-  var ans = Math.sqrt(1);
+var p;
+for(p= 2; p<10; p++){
+  var ans = Math.sqrt(p);
   document.write("the square is:" + ans + "<br />");
 }
